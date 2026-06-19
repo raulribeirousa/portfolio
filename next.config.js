a2194335e6c810ec859bbf9392207225ad0c137d
raulribeirousa <rraulm@ensign.edu>
2026-06-19 02:35:51 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portfolio',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
